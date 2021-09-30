33c0e4d7ee9d2f3444dc651bff2403e79f808064
Sulushash Shumenova <shumenova@Sulushashs-MacBook-Air.local>
2021-09-30 16:07:06 +0600

    solved problems

--- src/count-cats.js
+++ src/count-cats.js
@@ -15,15 +15,15 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  */
 export default function countCats(matrix) {
-  // let cnt = 0;
-  // for (let i = 0; i < matrix?.length; i ++) {
-  //   for (let j = 0; j < matrix[i]?.length; j++) {
-  //     if(matrix[i][j] === '^^') {
-  //       cnt++;
-  //     }
-  //   }
-  // }
+  let cnt = 0;
+  for (let i = 0; i < matrix?.length; i ++) {
+    for (let j = 0; j < matrix[i]?.length; j++) {
+      if(matrix[i][j] === '^^') {
+        cnt++;
+      }
+    }
+  }
 
-  // return cnt;
+  return cnt;
 }
 
