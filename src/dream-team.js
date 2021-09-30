e3f770866c4dd0b50e22fdb6bd6cca1cf53bda51
Sulushash Shumenova <shumenova@Sulushashs-MacBook-Air.local>
2021-09-30 15:30:25 +0600

    solved problems

--- src/dream-team.js
+++ src/dream-team.js
@@ -40,16 +40,3 @@ export default function createDreamTeam(members) {
   else return false;
 
 }
-// createDreamTeam([
-//   ['David Abram'],
-//   ['Robin Attfield'],
-//   'Thomas Berry',
-//   ['Paul R.Ehrlich'],
-//   'donna Haraway',
-//   ' BrIaN_gOodWiN  ',
-//   {
-//     0: 'Serenella Iovino'
-//   },
-//   'Erazim Kohak',
-//   '  val_plumwood',
-// ])
